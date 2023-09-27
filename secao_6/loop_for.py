f05fd9c3c4db77edf26b84a655b737650f989942
Eduardo <eduardo.sballico@gmail.com>
2023-09-26 22:05:00 -0300

    atualizando repositório

--- secao_6/loop_for.py
+++ secao_6/loop_for.py
@@ -29,11 +29,15 @@ nome = "Eduardo Salvagni Ballico"
 lista = [1, 3, 5, 7, 9]
 
 numeros = range(1, 10)
+
+# Range
 """
-range(valor_inicial, valor_final)
+range(valor_inicial, valor_final, passo)
 OBS: O valor final não é incluído
 """
 
+# Exemplos simples
+"""
 for letra in nome:
     print(letra)
 
@@ -42,3 +46,20 @@ for item in lista:
 
 for numero in numeros:
     print(numero)
+"""
+
+# Enumerate
+"""
+Método que itera algo e retorna o valor e o indíce do valor em uma tupla
+
+lista = [1, 3, 5, 7, 9]
+
+valor = [valor for valor in enumerate(lista)]
+print(valor) -> [(0, 1), (1, 3), (2, 5), (3, 7), (4, 9)]
+
+ou então separando e retornando em duas variáveis diferentes
+
+valor = [f"índíce: {i} -> valor: {v}" for i, v in enumerate(lista)]
+print(valor) -> ['índíce: 0 -> valor: 1', 'índíce: 1 -> valor: 3',
+                 'índíce: 2 -> valor: 5', 'índíce: 3 -> valor: 7', 'índíce: 4 -> valor: 9']
+"""
